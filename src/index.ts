e43f2622e18b828e91351f70c4a66cd58e6f18d1
Garret Harp <garretcharp@gmail.com>
2022-09-27 15:32:26 -0500

    things

--- src/index.ts
+++ src/index.ts
@@ -32,16 +32,6 @@ app.get('/do/d1', async c => {
 	}
 })
 
-app.get('/do/list', async c => {
-	try {
-		return c.env.TestDO.get(
-			c.env.TestDO.idFromName('1')
-		).fetch('https://fake-host/list')
-	} catch (error: any) {
-		return c.json({ error: error.message }, 500)
-	}
-})
-
 app.get('/do/websocket', async c => {
 	try {
 		return c.env.TestDO.get(
@@ -80,95 +70,6 @@ export class TestDO implements DurableObject {
 	private sockets = new Map<string, WebSocket>()
 
 	constructor(private state: DurableObjectState, private env: Bindings) {
-		this.state.blockConcurrencyWhile(async () => {
-			const items = await this.state.storage.list({ limit: 1 })
-
-			if (items.size === 0) {
-				await this.state.storage.put({
-					A: 'A', B: 'B', C: 'C', D: 'D', E: 'E',
-					F: 'F', G: 'G', H: 'H', I: 'I', J: 'J',
-					K: 'K', L: 'L', M: 'M', N: 'N', O: 'O',
-					P: 'P', Q: 'Q', R: 'R', S: 'S', T: 'T',
-					U: 'U', V: 'V', W: 'W', X: 'X', Y: 'Y',
-					Z: 'Z'
-				})
-			}
-		})
-
-		this.app.get('/list', async c => {
-			const list = await this.state.storage.list({})
-
-			const listReverse = await this.state.storage.list({
-				reverse: true
-			})
-
-			const listStartAfterE = await this.state.storage.list({
-				startAfter: 'E'
-			})
-
-			// startAfter becomes "end" when reverse is true
-			const listStartAfterEReverse = await this.state.storage.list({
-				end: 'E',
-				reverse: true
-			})
-
-			const listStartAfterQLimit5 = await this.state.storage.list({
-				startAfter: 'Q',
-				limit: 5
-			})
-
-			const listStartAfterQLimit5Reverse = await this.state.storage.list({
-				end: 'Q',
-				limit: 5,
-				reverse: true
-			})
-
-			const listStartAfterEEndM = await this.state.storage.list({
-				startAfter: 'E',
-				end: 'M'
-			})
-
-			const listStartAfterEEndMReverse = await this.state.storage.list({
-				startAfter: 'E',
-				end: 'M',
-				reverse: true
-			})
-
-			const listEndE = await this.state.storage.list({
-				end: 'E'
-			})
-
-			// end becomes "startAfter" when reverse is true
-			const listEndEReverse = await this.state.storage.list({
-				startAfter: 'E',
-				reverse: true
-			})
-
-			const listLimit3 = await this.state.storage.list({
-				limit: 3
-			})
-
-			const listLimit3Reverse = await this.state.storage.list({
-				limit: 3,
-				reverse: true
-			})
-
-			return c.json({
-				list: Array.from(list.keys()),
-				listReverse: Array.from(listReverse.keys()),
-				listStartAfterE: Array.from(listStartAfterE.keys()),
-				listStartAfterEReverse: Array.from(listStartAfterEReverse.keys()),
-				listStartAfterQLimit5: Array.from(listStartAfterQLimit5.keys()),
-				listStartAfterQLimit5Reverse: Array.from(listStartAfterQLimit5Reverse.keys()),
-				listStartAfterEEndM: Array.from(listStartAfterEEndM.keys()),
-				listStartAfterMEndEReverse: Array.from(listStartAfterEEndMReverse.keys()),
-				listEndE: Array.from(listEndE.keys()),
-				listEndEReverse: Array.from(listEndEReverse.keys()),
-				listLimit3: Array.from(listLimit3.keys()),
-				listLimit3Reverse: Array.from(listLimit3Reverse.keys())
-			})
-		})
-
 		this.app.get('/d1-in-do', async c => {
 			try {
 				const statement = c.env.TestD1.prepare('SELECT * FROM Test')
@@ -194,15 +95,45 @@ export class TestDO implements DurableObject {
 			connection.accept()
 			this.sockets.set(requestId, connection)
 
-			connection.addEventListener('message', (message) => {
+			connection.addEventListener('message', async (message) => {
 				if (typeof message.data === 'string') {
-					for (const socket of this.sockets.values()) {
-						socket.send(`[${requestId}]: ${message.data}`)
+					const [cmd, ...data] = message.data.split(' ')
+
+					switch (cmd) {
+						case '/list':
+							const messages = await this.state.storage.list({
+								prefix: 'Message/',
+								reverse: true,
+								limit: 100,
+								allowConcurrency: true,
+								noCache: true
+							})
+							connection.send(JSON.stringify(Array.from(messages.values())))
+							break
+						case '/send':
+							const id = crypto.randomUUID(), createdAt = new Date().toISOString()
+
+							await this.state.storage.put(`Message/${createdAt}/${id}`, {
+								id,
+								createdAt,
+								message: data.join(' '),
+								sender: requestId
+							}, {
+								allowConcurrency: true,
+								noCache: true
+							})
+
+							for (const socket of this.sockets.values()) {
+								socket.send(`[${requestId}]: ${data.join(' ')}`)
+							}
+
+							break
+						default:
+							connection.send('Invalid command. Must be one of [/list, /send]')
+							break
 					}
 				} else {
-					for (const socket of this.sockets.values()) {
-						socket.send(`[${requestId}]: idk it sent something`)
-					}
+					connection.send('Error: send messages as strings')
 				}
 			})
 
